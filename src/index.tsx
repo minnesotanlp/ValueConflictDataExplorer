25b92c6df3ac752ef8590d4aa376f4263a008672
JianingWen <jwen0808@outlook.com>
2025-03-16 23:02:50 -0500

    fix path

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import { SideBar } from "./SideBar";
 
 import { useStyles, theme } from "./style";
 
-export const basePath = process.env.NODE_ENV === 'development' ? '' : '/ValueConflictDataExplorer/';
+export const basePath = process.env.NODE_ENV === 'development' ? '' : '/ValueConflictDataExplorer';
 
 //Use tags dynamically
 let allTags = {};
